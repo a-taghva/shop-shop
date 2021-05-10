ab352208d2d17c633b30055b54a576fdf4e17937
a-taghva <taghvali99@gmail.com>
2021-05-10 08:09:51 -0400

    StoreProvider

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext } from 'react';
+import { useProductReducer } from './reducer';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext; 
+
+const StoreProvider = ({ value = [], ...props }) => {
+  // state is the most up-to-date version of global state object
+  // dispatch is the method we execute to update our state
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
+
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
